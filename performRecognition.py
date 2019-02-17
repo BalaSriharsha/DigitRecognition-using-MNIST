9433f87db7ae3a67dfdce8b53cc54d71211329b9
BalaSriharsha <balasriharshacheeday@gmail.com>
2019-02-17 16:06:17 +0530

    Update performRecognition.py

--- performRecognition.py
+++ performRecognition.py
@@ -1,7 +1,9 @@
+import numpy.core.multiarray
 import cv2
 from sklearn.externals import joblib
 from skimage.feature import hog
 import numpy as np
+import copy as cp
 
 clf = joblib.load("digits_cls.pkl")
 
@@ -12,32 +14,64 @@ im_gray = cv2.GaussianBlur(im_gray, (5, 5), 0)
 
 ret, im_th = cv2.threshold(im_gray, 90, 255, cv2.THRESH_BINARY_INV)
 
-_, ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+ctrs, hier = cv2.findContours(im_th.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 rects = [cv2.boundingRect(ctr) for ctr in ctrs]
 
-cor_sum = []
+xs = []
+ys = []
 for i in rects:
-    cor_sum.append(i[0]+i[1])
-cor_dic = dict(zip(cor_sum,rects))
-vals = []
-for i,j in cor_dic.items():
-    vals.append(i)
-vals.sort()
-vds = []
-for i in vals:
-    vds.append(cor_dic[i])
+    xs.append(i[0])
+    ys.append(i[1])
+xs_sorted = sorted(xs)
+ys_sorted = sorted(ys)
+least_x = xs_sorted[0]
+least_y = ys_sorted[0]
 
-q = vds[0][1]
+ys_sorted_modified = []
+for i in ys_sorted:
+    if(i<=least_y+50 and i>least_y-50):
+        ys_sorted_modified.append(least_y)
+    else:
+        least_y = i
 
-vs = []
-for i in vds:
-    if i[1]<=q+50 and i[1]>=q-50:
-        vs.append(i)
+ys_sorted_modified = list(set(ys_sorted_modified))
+
+sorted_rects = []
+
+
+lines = []
+
+for i in ys_sorted_modified:
+    line = []
+    for j in rects:
+        if(j[1]<=i+50 and j[1]>=i-50):
+            line.append(j)
+    lines.append(line)
+
+def sort_line(line):
+    xs = []
+    for i in line:
+        xs.append(i[0])
+    xs.sort()
+    sorted_line = []
+    for i in xs:
+        for j in line:
+            if(j[0]==i):
+                sorted_line.append(j)
+    return sorted_line
+
+page = []
+for i in lines:
+    page.append(sort_line(i))
+
+final_rects = []
+for i in page:
+    for j in i:
+        final_rects.append(j)
 
 nums = []
-
-for rect in vs:
+for rect in final_rects:
     cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3) 
     leng = int(rect[3] * 1.6)
     pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
@@ -50,10 +84,7 @@ for rect in vs:
     nums.append(nbr[0])
     cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]),cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
 
-#print(vals)
+
 print(nums)
-#print(vds)
-#print(cor_dic)
-#print(q)
 cv2.imshow("Resulting Image with Rectangular ROIs", im)
 cv2.waitKey()
